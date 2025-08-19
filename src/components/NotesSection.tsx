6d732adb8f527169c7a0ef945445bf68fbce5e0d
[dyad] <mehdikacim@hotmail.fr>
2025-08-19 20:27:45 +0200

    [dyad] wrote 1 file(s)

--- src/components/NotesSection.tsx
+++ src/components/NotesSection.tsx
@@ -84,7 +84,7 @@ const NotesSection = ({ noteKey, title, refreshKey }: NotesSectionProps) => {
       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
       [{ 'list': 'ordered' }, { 'list': 'bullet' }],
       [{ 'indent': '-1' }, { 'indent': '+1' }],
-      ['link', 'image'],
+      ['link', 'image', 'code-block'], // Added 'code-block'
       ['clean']
     ],
   };
@@ -93,7 +93,7 @@ const NotesSection = ({ noteKey, title, refreshKey }: NotesSectionProps) => {
     'header',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
-    'link', 'image'
+    'link', 'image', 'code-block' // Added 'code-block'
   ];
 
   return (
