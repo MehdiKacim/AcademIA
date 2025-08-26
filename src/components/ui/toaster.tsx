8b491985e74b48acbff374c53dbea6ae7b28e40c
[dyad] <git@dyad.sh>
2025-08-26 19:18:43 +0200

    [dyad] Correction de l'erreur React.Children.only dans le composant Toaster - wrote 1 file(s)

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -15,27 +15,23 @@ export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <React.Fragment>
-      <ToastProvider>
-        {/* Envelopper le résultat de .map() dans un Fragment pour que ToastProvider reçoive un seul enfant */}
-        <React.Fragment>
-          {toasts.map(function ({ id, title, description, action, ...props }) {
-            return (
-              <Toast key={id} {...props}>
-                <div className="grid gap-1">
-                  {title && <ToastTitle>{title}</ToastTitle>}
-                  {description && (
-                    <ToastDescription>{description}</ToastDescription>
-                  )}
-                </div>
-                {action}
-                <ToastClose />
-              </Toast>
-            )
-          })}
-        </React.Fragment>
-      </ToastProvider>
-      <ToastViewport />
-    </React.Fragment>
+    <ToastProvider>
+      <ToastViewport>
+        {toasts.map(function ({ id, title, description, action, ...props }) {
+          return (
+            <Toast key={id} {...props}>
+              <div className="grid gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
+              {action}
+              <ToastClose />
+            </Toast>
+          )
+        })}
+      </ToastViewport>
+    </ToastProvider>
   )
 }
