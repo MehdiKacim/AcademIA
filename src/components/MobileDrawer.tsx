3c9f02bdd9c36588622cfc28bafb0411bdee5ab1
[dyad] <git@dyad.sh>
2025-08-28 19:14:41 +0200

    [dyad] Correction de la superposition du tiroir mobile avec la barre de navigation inférieure. - wrote 1 file(s)

--- src/components/MobileDrawer.tsx
+++ src/components/MobileDrawer.tsx
@@ -53,7 +53,8 @@ const MobileDrawer = ({ isOpen, onClose, children, className, overlayClassName, 
           {/* Drawer Content */}
           <motion.div
             className={cn(
-              "fixed bottom-0 left-0 right-0 w-full h-[calc(100vh-68px)] bg-background rounded-t-lg shadow-lg flex flex-col overflow-hidden rounded-android-tile z-[999]", // Actual drawer content, highest z-index
+              "fixed left-0 right-0 w-full bg-background rounded-t-lg shadow-lg flex flex-col overflow-hidden rounded-android-tile z-[999]", // Actual drawer content, highest z-index
+              "bottom-[68px] h-[calc(100vh-136px)]", // Adjusted bottom and height to be above the 68px bottom nav and below the 68px top header
               contentClassName
             )}
             variants={contentVariants}
