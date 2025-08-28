fd49c2d24eb12755c779384d399da294ae3b777a
Mehdi Kacim <mehdikacim@hotmail.fr>
2025-08-28 11:19:59 +0200

    --

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,13 +4,6 @@ const config: CapacitorConfig = {
     appId: 'com.academia.app',
     appName: 'AcademIA',
     webDir: 'dist',
-    plugins: {
-        LiveUpdates: {
-            appId: '60f83a8a',
-            channel: 'Production',
-            autoUpdateMethod: 'background',
-        },
-    },
 };
 
 export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { Capacitor } from "@capacitor/core";
+
 
 // Lance la vérification et le téléchargement des mises à jour
-Capacitor.LiveUpdates.sync();
 
 createRoot(document.getElementById("root")!).render(<App />);
