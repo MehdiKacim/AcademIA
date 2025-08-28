ffc835777720e6b3cb93503bf7ea311e78b3bfc1
Mehdi Kacim <mehdikacim@hotmail.fr>
2025-08-28 11:02:10 +0200

    --

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { LiveUpdates } from "@capacitor/live-updates";
+import  LiveUpdates from "@capacitor/live-updates";
 
 // console.log("[main.tsx] Application starting up.");
 // Lance la vérification et le téléchargement des mises à jour
